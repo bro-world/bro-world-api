bc0e2a5cdeb79d64fbc118a589d33e2b211e2a1b
Rami <rami.aouinti@gmail.com>
2026-03-11 22:49:52 +0100

    calendar: scope application-linked routes by slug and harden private scope

--- src/Calendar/Application/MessageHandler/PatchEventCommandHandler.php
+++ src/Calendar/Application/MessageHandler/PatchEventCommandHandler.php
@@ -44,6 +44,8 @@ final readonly class PatchEventCommandHandler
                 if ($event->getCalendar()?->getApplication()?->getId() !== $application->getId()) {
                     throw new HttpException(JsonResponse::HTTP_NOT_FOUND, 'Event not found.');
                 }
+            } elseif ($event->getCalendar()?->getApplication() !== null) {
+                throw new HttpException(JsonResponse::HTTP_NOT_FOUND, 'Event not found.');
             }
 
             if ($command->title !== null) {
