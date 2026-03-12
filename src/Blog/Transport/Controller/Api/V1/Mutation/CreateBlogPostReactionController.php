c592f8626dc74f68a1320702deb67b343aad74a1
Rami <rami.aouinti@gmail.com>
2026-03-12 01:26:13 +0100

    Return id when creating blog post reactions

--- src/Blog/Transport/Controller/Api/V1/Mutation/CreateBlogPostReactionController.php
+++ src/Blog/Transport/Controller/Api/V1/Mutation/CreateBlogPostReactionController.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Blog\Transport\Controller\Api\V1\Mutation;
 
 use App\Blog\Application\Message\CreateBlogPostReactionCommand;
+use App\Blog\Application\MessageHandler\CreateBlogPostReactionCommandHandler;
 use App\Blog\Application\Service\BlogMutationRequestService;
 use App\User\Domain\Entity\User;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Messenger\Exception\ExceptionInterface;
-use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authorization\Voter\AuthenticatedVoter;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -23,22 +22,19 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final readonly class CreateBlogPostReactionController
 {
     public function __construct(
-        private MessageBusInterface $messageBus,
+        private CreateBlogPostReactionCommandHandler $handler,
         private BlogMutationRequestService $requestService,
     ) {
     }
-
-    /**
-     * @throws ExceptionInterface
-     */
     #[Route('/v1/private/blog/posts/{postId}/reactions', methods: [Request::METHOD_POST])]
     public function __invoke(string $postId, Request $request, User $loggedInUser): JsonResponse
     {
         $payload = $this->requestService->extractPayload($request);
-        $this->messageBus->dispatch(new CreateBlogPostReactionCommand((string)uniqid('op_', true), $loggedInUser->getId(), $postId, $this->requestService->parseReactionType((string)($payload['type'] ?? 'like'))));
+        $entityId = $this->handler->__invoke(new CreateBlogPostReactionCommand((string)uniqid('op_', true), $loggedInUser->getId(), $postId, $this->requestService->parseReactionType((string)($payload['type'] ?? 'like'))));
 
         return new JsonResponse([
             'status' => 'accepted',
+            'id' => is_string($entityId) ? $entityId : null,
         ], JsonResponse::HTTP_ACCEPTED);
     }
 }
